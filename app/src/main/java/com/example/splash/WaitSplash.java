ec608715b7d76d3d13781c6691978547de49fa6a
clopeclever <cleverarinda@gmail.com>
2020-02-27 11:53:25 +0300

    commit message

---
+++ app/src/main/java/com/example/splash/WaitSplash.java
@@ -0,0 +1,14 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaitSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait_splash);
+    }
+}
